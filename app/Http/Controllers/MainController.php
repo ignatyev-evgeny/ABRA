2e2b54257ce7a2610d8e5a0cf70ea2d031ef3636
Evgeny Ignatyev <work@ignatyev.pro>
2023-06-29 20:37:31 +0300

    MainContoller update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class MainController extends Controller {
 
+    /** Homepage index */
     public function index() {
         return view('main');
     }
 
+    /** User basket */
+    public function basket() {
+        return view('basket');
+    }
+
+    /** Sign in page */
     public function singin() {
         return view('authorization.singin');
     }
 
+    /** Sign up page */
     public function singup() {
         return view('authorization.signup');
+    }
+
+    /** Logout */
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
