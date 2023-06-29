5e22079bcff1b15ae7f17301d66a2710dc0bc7dc
Evgeny Ignatyev <work@ignatyev.pro>
2023-06-29 20:28:43 +0300

    Livewire component register user

--- app/Http/Livewire/Authorization/SignUpUser.php
+++ app/Http/Livewire/Authorization/SignUpUser.php
@@ -16,39 +16,45 @@ class SignUpUser extends Component {
     public $userPassword;
     public $signUpError;
 
+    /** Validation rules */
     protected $rules = [
         'name' => 'required|min:10|max:100',
         'email' => 'required|email|unique:users,email',
     ];
 
-    public function signUp() {
+    /** User registration method */
+    public function signUp(): void {
 
         $this->validate();
-        $this->userPassword = uuid_create();
+        $this->userPassword = fake()->unique()->password(10);
 
         try {
+
             $newUser = User::create([
                 'name' => $this->name,
-                //            'email' => $this->email,
-                'email' => 'togojise@mailinator.com',
-                'password' => Hash::make(md5($this->userPassword), [
-                    'rounds' => 12,
-                ])
+                'email' => $this->email,
+                'password' => Hash::make($this->userPassword)
             ]);
+
             $this->status = (bool) $newUser;
+
         } catch (Exception $exception) {
-            Log::channel('signup')->critical("{$this->name} | {$this->email} | {$this->userPassword} - {$exception->getLine()} | {$exception->getMessage()}");
-            $this->signUpError = __("User <b>{$this->name}</b> registration error");
+
+            Log::channel('signup')->critical("$this->name | $this->email | $this->userPassword - {$exception->getLine()} | {$exception->getMessage()}");
+            $this->signUpError = __("User <b>$this->name</b> registration error");
+
         }
 
     }
 
     public function render() {
+
         return view('livewire.authorization.sign-up-user', [
             'status' => $this->status,
             'userEmail' => $this->email,
             'userPassword' => $this->userPassword,
         ]);
+
     }
 
 }
