f85fc78be7b4f2524ae9d1366cdfed204b261880
Evgeny Ignatyev <work@ignatyev.pro>
2023-06-29 20:23:06 +0300

    Routing settings

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BasketController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/** Homepage */
 Route::get('/', [MainController::class, 'index'])->name('main.index');
-Route::get('/signin', [MainController::class, 'singin'])->name('main.signin');
-Route::get('/signup', [MainController::class, 'singup'])->name('main.signup');
+
+/** Sign in page */
+Route::get('/signin', [MainController::class, 'singin'])->middleware(['guest'])->name('main.signin');
+
+/** Sign up page */
+Route::get('/signup', [MainController::class, 'singup'])->middleware(['guest'])->name('main.signup');
+
+/** User basket page */
+Route::get('/basket', [MainController::class, 'basket'])->name('main.basket');
+
+/** Logout page */
+Route::get('/logout', [MainController::class, 'logout'])->middleware(['auth'])->name('main.logout');
+
+/** Area requiring authorization */
+Route::middleware(['auth'])->prefix('cabinet')->name('cabinet.')->group(function () {
+
+    /** Goods group */
+    Route::prefix('product')->name('product.')->group(function () {
+        /** Goods list */
+        Route::get('/list', [ProductController::class, 'list'])->name('list');
+    });
+
+    /** Baskets group */
+    Route::prefix('baskets')->name('baskets.')->group(function () {
+        /** Baskets list */
+        Route::get('/list', [BasketController::class, 'list'])->name('list');
+    });
+
+});
